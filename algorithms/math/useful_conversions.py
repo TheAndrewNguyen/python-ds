d436b7e34abea152b0dbdba59eb4cda540de087f
Andrew Bao-Viet Andrew Bao-Viet <andrewbvietnguyen@gmail.com>
2020-10-24 19:23:08 +0000

    I added some useful math conversions

---
+++ algorithms/math/useful_conversions.py
@@ -0,0 +1,19 @@
+#algortihms for useful math and science conversions    
+
+#inches to feet 
+def inches_feet(i): 
+  return i * 0.0833333    
+
+def feet_inches(f): 
+  return f * 12 
+
+
+#Kelvins to celsuis 
+def kelvin_celsuis(k):           
+  return k - 273.15    
+
+#celsuis to kelvins 
+def celsuis_kelvin(c): 
+  return c + 273.15    
+
+
